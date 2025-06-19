bfeed20456a06a06c0db1fa3dbafc883ffa0b60c
shinjir0u <htooagwai9@gmail.com>
2025-06-19 16:07:39 +0630

    Setup lightning css for css module

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { browserslistToTargets } from "lightningcss";
+import browserslist from "browserslist";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,14 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
+  },
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(">=0.25%")),
+    },
+  },
+  build: {
+    cssMinify: "lightningcss",
   },
 });

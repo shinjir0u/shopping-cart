c1cd224f568b1ba2eb342067b716440d3cb66cee
shinjir0u <htooagwai9@gmail.com>
2025-06-19 19:15:17 +0630

    Fix bugs: setup errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import routes from "./routes";
 
-const router = createBrowserRouter(router);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
